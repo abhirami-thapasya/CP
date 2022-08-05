d388cc457a9da65ffd07913e1124b8d20d76d8e7
abhirami-thapasya <abhiramithapasya@gmail.com>
2022-08-05 18:45:52 +0530

    codechef-ZCO14003-Smart Phone

---
+++ Smart Phone.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    long long n,maxRev=0;
+    cin>>n;
+    long long a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    for(int i=0;i<n;i++){
+        if(a[i]*(n-i)>maxRev){
+            maxRev=a[i]*(n-i);
+        }
+    }
+    cout<<maxRev<<endl;
+    return 0;
+}
