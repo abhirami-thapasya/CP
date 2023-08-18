8c8ab5966116b97b1c07f9a0fa373518531223a4
abhirami-thapasya <abhiramithapasya@gmail.com>
2023-08-18 15:34:36 +0530

    LeetCode-224. Basic Calculator

---
+++ 224. Basic Calculator.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int sum=0;
+        int sign=1;
+        stack<int> st;
+        for(int i=0;i<s.size();i++){
+            if(isdigit(s[i])){
+                int val=0;
+                while(i<s.size() && isdigit(s[i])){
+                    val=(val*10)+(s[i]-'0');
+                    i++;
+                }
+                i--;
+                sum+=val*sign;
+                sign=1;
+            }
+            else if(s[i]=='-'){
+                sign*=-1;
+            }
+            else if(s[i]=='('){
+                st.push(sum);
+                st.push(sign);
+                sum=0;
+                sign=1;
+            }
+            else if(s[i]==')'){
+                sum*=st.top();
+                st.pop();
+                sum+=st.top();
+                st.pop();
+            }
+        }
+        return sum;
+    }
+};
