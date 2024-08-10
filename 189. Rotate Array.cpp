ae83644dffd82a9643ebc4fb98ac75c6a1711398
abhirami-thapasya <abhiramithapasya@gmail.com>
2024-08-10 23:07:03 +0530

    Leetcode-189. Rotate Array

---
+++ 189. Rotate Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k%=nums.size();
+        int i=0,j=nums.size()-1-k;
+        while(i<j){
+            swap(nums[i++],nums[j--]);
+        }
+        i=nums.size()-k;
+        j=nums.size()-1;
+        while(i<j){
+            swap(nums[i++],nums[j--]);
+        }
+        i=0;
+        j=nums.size()-1;
+        while(i<j){
+            swap(nums[i++],nums[j--]);
+        }
+    }
+};
