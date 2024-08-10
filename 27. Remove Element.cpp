23c5f92ab6cbf0a85d3520b343fd21bb3b646ded
abhirami-thapasya <abhiramithapasya@gmail.com>
2024-08-10 13:38:45 +0530

    Leetcode-27. Remove Element-100% beats

--- 27. Remove Element.cpp
+++ 27. Remove Element.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int count=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=val){
-                nums[count++]=nums[i];
+        for(auto n:nums){
+            if(n!=val){
+                nums[count++]=n;
             }
         }
         return count;
